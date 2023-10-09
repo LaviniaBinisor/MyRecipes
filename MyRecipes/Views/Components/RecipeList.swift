cee9e7e6048db9dc3854aec4a961714b61bdb8ef
Binisor Maria Lavinia <binisor.lavinia@gmail.com>
2023-10-09 14:32:42 +0300

    Create Recipe List

--- MyRecipes/Views/Components/RecipeList.swift
+++ MyRecipes/Views/Components/RecipeList.swift
@@ -2,17 +2,38 @@
 //  RecipeList.swift
 //  MyRecipes
 //
-//  Created by ionut.binisor on 05.10.2023.
+//  Created by lavinia.binisor on 05.10.2023.
 //
 
 import SwiftUI
 
 struct RecipeList: View {
+    var recipes: [Recipe]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("\(recipes.count) \(recipes.count > 1 ? "recipes" : "recipe")")
+                .font(.headline)
+                .foregroundStyle(.primary)
+                .fontWeight(.medium)
+            
+            Spacer()
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 150), spacing: 2)], spacing: 20) {
+                ForEach(recipes) { recipe in
+                    NavigationLink(destination: RecipeView(recipe: recipe)) {
+                        RecipeCard(recipe: recipe)
+                    }
+                }
+            }
+            .padding(.top)
+        }
+        .padding(.horizontal)
     }
 }
 
-#Preview {
-    RecipeList()
+struct RecipeList_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeList(recipes: Recipe.allRecipes)
+    }
 }
---
+++ MyRecipes/Views/Details/RecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeView.swift
+//  MyRecipes
+//
+//  Created by ionut.binisor on 06.10.2023.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeView()
+}
